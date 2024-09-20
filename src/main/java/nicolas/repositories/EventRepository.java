728927d8eeba1443d05e9e2754471fa8d07e3f7f
Nicolas P. <168541456+nikeniko@users.noreply.github.com>
2024-09-20 14:17:07 +0200

    repository

---
+++ src/main/java/nicolas/repositories/EventRepository.java
@@ -0,0 +1,8 @@
+package nicolas.repositories;
+
+
+import nicolas.entity.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EventRepository extends JpaRepository<Event, Long> {
+}
---
+++ src/main/java/nicolas/repositories/RoleRepository.java
@@ -0,0 +1,12 @@
+package nicolas.repositories;
+
+
+import nicolas.entity.Role;
+import nicolas.entity.Role.RoleName;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(RoleName roleName);
+}
