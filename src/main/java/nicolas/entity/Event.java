cc34f931f8d419151a1b4c974b81834fae43ef6e
Nicolas P. <168541456+nikeniko@users.noreply.github.com>
2024-09-20 14:01:57 +0200

    entità

---
+++ src/main/java/nicolas/entity/Event.java
@@ -0,0 +1,31 @@
+package nicolas.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Event {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String description;
+    private LocalDateTime date;
+    private String location;
+    private int availableSeats;
+
+    @ManyToOne
+    @JoinColumn(name = "organizer_id")
+    private User organizer;
+}
---
+++ src/main/java/nicolas/entity/Role.java
@@ -0,0 +1,26 @@
+package nicolas.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private RoleName name;
+
+    public enum RoleName {
+        ROLE_USER, ROLE_ORGANIZER
+    }
+}
